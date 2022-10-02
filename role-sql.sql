127154bb3ca221347eab093e66854226fbe57a1a
Minh <81819853+IkMinhTon@users.noreply.github.com>
2022-10-02 17:34:55 +0700

    Add role field sql

---
+++ role-sql.sql
@@ -0,0 +1,5 @@
+INSERT [dbo].[role] ([role_name]) VALUES (N'CANDIDATE')
+INSERT [dbo].[role] ([role_name]) VALUES (N'HREMPLOYEE')
+INSERT [dbo].[role] ([role_name]) VALUES (N'HRMANAGER')
+INSERT [dbo].[role] ([role_name]) VALUES (N'INTERVIEWER')
+INSERT [dbo].[role] ([role_name]) VALUES (N'ADMIN')
